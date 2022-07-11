6cb2831678701959f65593bf29c4466ece20b96f
Alejandro Quintanar <alejandro@quihel.net>
2022-07-11 19:18:07 +0200

    Testing pygame full screen

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import pygame, sys, os
 from pygame.locals import *
 
-pygame.display.init()
-pygame.display.list_modes()
-display = pygame.display.set_mode((1280, 720))
+pygame.init()
+display_info = pygame.display.Info()
+display = pygame.display.set_mode((display_info.current_w, display_info.current_h))
 pygame.display.set_caption('Hello World!')
-while True: # main game loop
+while True:  # main game loop
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
