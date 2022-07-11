c8cb3ccf8a2c1774aee279056677550fa3585f18
Alejandro Quintanar <alejandro@quihel.net>
2022-07-11 19:28:45 +0200

    Testing Windows Name

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from pygame.locals import *
 pygame.init()
 display_info = pygame.display.Info()
 display = pygame.display.set_mode((display_info.current_w, display_info.current_h))
-pygame.display.set_caption('Hello World!')
+pygame.display.set_caption('TUI Commander 0.1a')
+
 while True:  # main game loop
     for event in pygame.event.get():
         if event.type == QUIT:
